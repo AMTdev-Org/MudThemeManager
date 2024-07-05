be566bedd0b9f979e8a218184b098d75d7ae4ac4
AMTdev Alien <craig@amtdev.com>
2024-07-05 14:40:25 -0400

    initial commit of operational theme manager

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MudThemeManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
